b906a20e56fe5dfea3c440afe08d5b36518ef305
Muthoni2000 <bmuthoni39@yahoo.com>
2020-03-04 18:31:25 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// function submit(){
+//   var input1 = document.getElementById('name').value;
+//   var email = document.getElementById('email').value;
+//   var user = document.getElementById('message').value;
+//   if (input1==''|| email==''||user==''){
+//     alert('Invalid input');
+//   }
+//   else {
+//     alert ('Thanks ' +   input1 + " we have received your message. Thank you for reaching out to us.")
+//   }
+    
+// };
+function output(){
+  var name=document.getElementById("name").value;
+  var email= document.getElementById("email").value;
+  var user=document.getElementById("message").value;
+  if(name==""||email==""||user==""){
+    alert("invalid imput")
+  }
+  else{
+    alert ('Thanks ' + name + " we have received your message. Thank you for reaching out to us.")
+  }
+};
  $(document).ready(function(){
         $("#img1").click(function(){
           $("#img1").hide();
@@ -19,22 +42,45 @@
           $("#p3").toggle();
           $("#img3").show();
         });
-      }); 
-        $('.test').hover(function () {
-          $(this).animate({opacity:'1'});
-          },
-          function () {
-          $(this).animate({opacity:'0'});
-      });
-        $("button").click(function(event){
-          event.preventDefault();
-          var input1 = document.getElementById('name').value;
-          var email = document.getElementById('email').value;
-          var user = document.getElementById('contactform').reset();
-          if (input1 && email != ''){
-              alert('Thanks ' +   input1 + " we have received your message. Thank you for reaching out to us.");
-          }else {
-            alert ('Invalid input')
-          }
+      //   $('.test').hover(function () {
+      //     $(this).animate({opacity:'1'});
+      //     },
+      //     function () {
+      //     $(this).animate({opacity:'0'});
+      // });
+        // $("button#button").click(function(){
+        //   var input1 = document.getElementById('name').val;
+        //   var email = document.getElementById('email').val;
+        //   var user = document.getElementById('message').val;
+        //   $if (input1==""|| email==""||user==""){
+        //       alert('Invalid input');
+        //   }
+        //   $else {
+        //     alert ('Thanks ' +   input1 + " we have received your message. Thank you for reaching out to us.")
+        //   }
       
-           });
+        //    });
+          }); 
+
+$(document).ready(function(){
+  $("#image4").mouseover(function(){
+    $("#safe").show();
+  }).mouseout(function(){
+    $("#safe").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#image3").mouseover(function(){
+    $("#app").show();
+  }).mouseout(function(){
+    $("#app").hide();
+  });
+});
+$(document).ready(function(){
+  $("#image3").mouseover(function(){
+    $("#app").show();
+  }).mouseout(function(){
+    $("#app").hide();
+  });
+});
